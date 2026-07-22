19ff53f2020db1e36211642e521ca132b1ed5065
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-22 14:24:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-alert("Step 1 - app.js started");
+alert("APP JS WORKING");
 
-const supabaseUrl = "https://nkdzfxanmvmrhehqtrtl.supabase.co";
-
-const supabaseKey =
-"sb_publishable_0K8Tq7ng_CCm6wVrRBJxGQ_kNYhwaXq";
-
-alert("Step 2 - URL and Key loaded");
-
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
-
-alert("Step 3 - Supabase connected");
-
-const form = document.getElementById("registerForm");
-
-if (!form) {
-  alert("ERROR: registerForm not found");
-} else {
-  alert("Step 4 - Form found");
-
-  form.addEventListener("submit", async (e) => {
-
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function(e) {
     e.preventDefault();
-
-    alert("Step 5 - Submit clicked");
-
+    alert("FORM SUBMITTED");
   });
-}
