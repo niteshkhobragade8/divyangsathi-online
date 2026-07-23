a71e833338bfc0e9416a1517efa7f9e5fa56b0e0
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-23 12:31:46 +0530

    Integrate Supabase for user registration
    Updated the registration form submission to use Supabase for inserting user profiles and added error handling.

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
-alert("APP JS WORKING");
+const supabaseUrl = "https://nkdzfxanmvmrhehqtrtl.supabase.co";
+
+const supabaseKey =
+"sb_publishable_0K8Tq7ng_CCm6wVrRBJxGQ_kNYhwaXq";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
 
 document
-  .getElementById("registerForm")
-  .addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("FORM SUBMITTED");
-  });
+.getElementById("registerForm")
+.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const full_name =
+document.getElementById("full_name").value;
+
+const mobile =
+document.getElementById("mobile").value;
+
+const email =
+document.getElementById("email").value;
+
+const gender =
+document.getElementById("gender").value;
+
+const { error } = await supabase
+.from("profiles")
+.insert([
+{
+full_name: full_name,
+mobile: mobile,
+email: email,
+gender: gender
+}
+]);
+
+if(error){
+
+alert("Error: " + error.message);
+
+}else{
+
+alert("Registration Successful!");
+
+document
+.getElementById("registerForm")
+.reset();
+
+}
+
+});
