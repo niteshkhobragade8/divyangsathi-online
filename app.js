4fb52743f73e0bf8789259c6690bb29a7e2e3122
niteshkhobragade8 <niteshkhobragade8@gmail.com>
2026-07-23 13:41:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,8 @@
-alert("APP START");
-
-const supabaseUrl = "https://nkdzfxanmvmrhehqtrtl.supabase.co";
-
-const supabaseKey =
-"sb_publishable_0K8Tq7ng_CCm6wVrRBJxGQ_kNYhwaXq";
-
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
+alert("APP JS WORKING");
 
 document
-.getElementById("registerForm")
-.addEventListener("submit", async function(e){
-
-e.preventDefault();
-
-const full_name =
-document.getElementById("full_name").value;
-
-const mobile =
-document.getElementById("mobile").value;
-
-const email =
-document.getElementById("email").value;
-
-const gender =
-document.getElementById("gender").value;
-
-const { error } = await supabase
-.from("profiles")
-.insert([
-{
-full_name: full_name,
-mobile: mobile,
-email: email,
-gender: gender
-}
-]);
-
-if(error){
-
-alert("Error: " + error.message);
-
-}else{
-
-alert("Registration Successful!");
-
-document
-.getElementById("registerForm")
-.reset();
-
-}
-
-});
+  .getElementById("registerForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("FORM SUBMITTED");
+  });
